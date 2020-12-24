4ab5d5cd3ee0055b330c1aa0565829e9484e345d
neilalden <escobin.neil@gmail.com>
2020-12-24 20:09:36 +0800

    needs to add login, logs and sales feature

--- public/main.js
+++ public/main.js
@@ -4,58 +4,112 @@ const itemsRef = db.collection("items");
 
 const allItemsList = document.getElementById("allItemsList");
 const purchasedItemsList = document.getElementById("purchasedItemsList");
-const total = document.getElementById("total");
+const searchTextbox = document.getElementById("searchTextbox");
 const cancelBtn = document.getElementById("cancelBtn");
+const checkOutBtn = document.getElementById("checkOutBtn");
+const total = document.getElementById("total");
+// name, cost & quantity
+var allItemsArray = [];
+// search result in allItemsArray
+var resultAllItemsArray = [];
+// name, cost & quantity
+var purchasedItemsArray = [];
 
 // onload main function
 
 function main() {
 	// display all items in all items
 	itemsRef.onSnapshot((querySnapshot) => {
+		allItemsArray = [];
+
 		const items = querySnapshot.docs.map((doc) => {
 			const name = doc.data().item_name;
-			const cost = doc.data().item_cost;
 			const desc = doc.data().item_desc;
-			return (
-				`<li><button onclick="addItem(\`` +
-				name +
-				`\`,\`` +
-				desc +
-				`\`,\`` +
-				cost +
-				`\`)"><h3 >${name}</h3><p>${desc}</p></button></li>`
-			);
+			const cost = doc.data().item_cost;
+			allItemsArray.push({ name, desc, cost });
+			allItemsRender(allItemsArray);
 		});
-		allItemsList.innerHTML = items.join("");
 	});
 }
 main();
 
-let arrayList = [];
 function addItem(name, desc, cost) {
-	var index = arrayList.findIndex((item) => item.name == name);
+	var index = purchasedItemsArray.findIndex((item) => item.name == name);
 	if (index == -1) {
-		arrayList.push({ name, cost, quantity: 1 });
+		purchasedItemsArray.push({ name, cost, quantity: 1 });
 	} else {
-		arrayList[index].quantity += 1;
+		purchasedItemsArray[index].quantity += 1;
 	}
-	cartRender(arrayList);
+
+	purchasedItemsRender(purchasedItemsArray);
+}
+function removeItem(itemName) {
+	for (let i = 0; i < purchasedItemsArray.length; i++) {
+		if (purchasedItemsArray[i].name == itemName) {
+			purchasedItemsArray.splice(i, 1);
+		}
+	}
+	purchasedItemsRender(purchasedItemsArray);
 }
 cancelBtn.addEventListener("click", () => {
-	arrayList = [];
-	cartRender(arrayList);
+	purchasedItemsArray = [];
+	purchasedItemsRender(purchasedItemsArray);
+});
+checkOutBtn.addEventListener("click", () => {
+	purchasedItemsArray = [];
+	purchasedItemsRender(purchasedItemsArray);
+});
+searchTextbox.addEventListener("input", () => {
+	const value = searchTextbox.value;
+	const reg = new RegExp(value, "gi");
+	resultAllItemsArray = [];
+	for (let i = 0; i < allItemsArray.length; i++) {
+		if (allItemsArray[i].name.match(reg)) {
+			const name = allItemsArray[i].name;
+			const desc = allItemsArray[i].desc;
+			const cost = allItemsArray[i].cost;
+			resultAllItemsArray.push({ name, desc, cost });
+		}
+	}
+	allItemsRender(resultAllItemsArray);
 });
 
 //!!!!!!!!!!RENDER FUNCTION!!!!!!!!!!//
-function cartRender(items) {
+function purchasedItemsRender(items) {
 	purchasedItemsList.innerHTML = "";
 	total.innerHTML = "Total : ";
 	let overAllTotal = 0;
-	items.map((item) => {
-		purchasedItemsList.innerHTML += `<li>${item.name} ${item.quantity}  -  ${
-			item.quantity * item.cost
-		}</li>`;
+	const newItems = items.map((item) => {
+		//calling the function removeItem returns undefined unless item.name is stored in a variable
+		const name = item.name;
 		overAllTotal += item.quantity * item.cost;
+		return (
+			`<li><button id="removeItemBtn" onclick="removeItem(\`` +
+			name +
+			`\`)">
+			<h3 >${name}</h3><p>${item.quantity}  -  ${
+				item.quantity * item.cost
+			} </p></button></li> `
+		);
 	});
+	purchasedItemsList.innerHTML = newItems.join("");
 	total.innerHTML = "Total : " + overAllTotal;
+}
+function allItemsRender(items) {
+	const newItems = items.map((items) => {
+		const name = items.name;
+		const desc = items.desc;
+		const cost = items.cost;
+		return (
+			`<li><button onclick="addItem(\`` +
+			name +
+			`\`,\`` +
+			desc +
+			`\`,\`` +
+			cost +
+			`\`)"><h3 >${name}</h3><p>${desc}</p> </button></li>`
+		);
+	});
+
+	allItemsList.innerHTML = newItems.join("");
 }
